fc6eeb8fb1b4fe7a88681f2b53c35e9d2a9cf0d4
ryangleason <league@WorthyMini-6.attlocal.net>
2018-03-05 20:00:57 -0800

    IntroToJUniT (ip)

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -9,41 +9,57 @@ import org.junit.Test;
  * **/
 
 public class MoreAndMoreTests {
+	int multiply(int one, int two) {
+		return one*two;
+	}
+	@Test
+	public void MultTest() {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
+	    assertEquals("10 x 0 = 0", multiply(10, 0));
+	    assertEquals("10 x 10 = 100", multiply(10, 10));
+	    assertEquals("8 x 11 = 88", multiply(8, 11));
+	  }
+	boolean isPrime(int num) {
+		for (int i=num-1;i>0;i--) {
+			if (num%i==0) {
+				return true;
+			}
+		}
+		return false;
+	}
+	@Test
+	  public void PrimeTest() {
+
+	    assertTrue(isPrime(3));
+	    assertTrue(isPrime(5));
+	    assertTrue(isPrime(541));
+	    assertFalse(isPrime(4));
+	    assertFalse(isPrime(12));
+	    assertFalse(isPrime(527));
+
+	  }
+	boolean isSquare(int num) {
+		for (int i=num;i>0;i--) {
+			if (i*i==num) {
+				return true;
+			}
+		}
+		return false;
+	}
+	@Test
+	  public void SquareTest() {
+
+	    assertTrue(isSquare(4));
+	    assertTrue(isSquare(144));
+	    assertTrue(isSquare(64));
+	    assertTrue(isSquare(10201));
+	    assertTrue(isSquare(1));
+	    assertFalse(isSquare(3));
+	    assertFalse(isSquare(22));
+	    assertFalse(isSquare(143));
+
+	  }
+
 //	@Test
 //	  public void CubeTest() {
 //
