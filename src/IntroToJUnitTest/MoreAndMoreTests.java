6b777769ad14045d1d9f55133035467ec4af439a
m990 <league@WTS14.attlocal.net>
2018-03-12 20:01:01 -0700

    SearchingAndSorthingAlgorithms(ip)

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -1,78 +1,92 @@
 package IntroToJUnitTest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 /**
  * Uncomment each test and then write the method to make it pass.
- * **/
+ **/
 
 public class MoreAndMoreTests {
-	int multiply(int one, int two) {
-		return one*two;
+	String multiply(int one, int two) {
+		return "" + one + " x " + two + " = " + one * two;
 	}
+
 	@Test
 	public void MultTest() {
 
-	    assertEquals("10 x 0 = 0", multiply(10, 0));
-	    assertEquals("10 x 10 = 100", multiply(10, 10));
-	    assertEquals("8 x 11 = 88", multiply(8, 11));
-	  }
+		assertEquals("10 x 0 = 0", multiply(10, 0));
+		assertEquals("10 x 10 = 100", multiply(10, 10));
+		assertEquals("8 x 11 = 88", multiply(8, 11));
+	}
+
 	boolean isPrime(int num) {
-		for (int i=num-1;i>0;i--) {
-			if (num%i==0) {
-				return true;
+		for (int i = num - 1; i > 1; i--) {
+			if (num % i == 0) {
+				return false;
 			}
 		}
-		return false;
+		return true;
 	}
+
 	@Test
-	  public void PrimeTest() {
+	public void PrimeTest() {
 
-	    assertTrue(isPrime(3));
-	    assertTrue(isPrime(5));
-	    assertTrue(isPrime(541));
-	    assertFalse(isPrime(4));
-	    assertFalse(isPrime(12));
-	    assertFalse(isPrime(527));
+		assertTrue(isPrime(3));
+		assertTrue(isPrime(5));
+		assertTrue(isPrime(541));
+		assertFalse(isPrime(4));
+		assertFalse(isPrime(12));
+		assertFalse(isPrime(527));
 
-	  }
+	}
+
 	boolean isSquare(int num) {
-		for (int i=num;i>0;i--) {
-			if (i*i==num) {
+		for (int i = num; i > 0; i--) {
+			if (i * i == num) {
 				return true;
 			}
 		}
 		return false;
 	}
+
 	@Test
-	  public void SquareTest() {
+	public void SquareTest() {
 
-	    assertTrue(isSquare(4));
-	    assertTrue(isSquare(144));
-	    assertTrue(isSquare(64));
-	    assertTrue(isSquare(10201));
-	    assertTrue(isSquare(1));
-	    assertFalse(isSquare(3));
-	    assertFalse(isSquare(22));
-	    assertFalse(isSquare(143));
+		assertTrue(isSquare(4));
+		assertTrue(isSquare(144));
+		assertTrue(isSquare(64));
+		assertTrue(isSquare(10201));
+		assertTrue(isSquare(1));
+		assertFalse(isSquare(3));
+		assertFalse(isSquare(22));
+		assertFalse(isSquare(143));
 
-	  }
+	}
 
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	boolean isCube(int num) {
+		for (int i = 1; i <= num; i++) {
+			if (num == i * i * i || num == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
 
+	@Test
+	public void CubeTest() {
 
+		assertTrue(isCube(27));
+		assertTrue(isCube(216));
+		assertTrue(isCube(729));
+		assertTrue(isCube(1));
+		assertFalse(isCube(3));
+		assertFalse(isCube(22));
+		assertFalse(isCube(143));
+
+	}
 
 }
--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,8 +4,31 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		return eggs.indexOf("cracked");
 	}
-	
-	//Add other methods here
+
+	// Add other methods here
+	public static int countPearls(List<Boolean> oyster) {
+		int amountTrue = 0;
+		for (Boolean o : oyster) {
+			if (o == true) {
+				amountTrue++;
+			}
+		}
+		return amountTrue;
+	}
+
+	public static double findTallest(List<Double> doubleList) {
+		double biggest = 0;
+		for (double tallest : doubleList) {
+			if (tallest > biggest) {
+				biggest = tallest;
+			}
+		}
+		return biggest;
+	}
+
+	/*
+	 * public static String findLongestWord(List<String> wordList) { }
+	 */
 }
